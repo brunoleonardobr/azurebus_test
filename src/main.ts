37105fde192816f60518eb230ce61e9aa59f76b1
Bruno Leonardo Fagundes <brunoleonardodev@gmail.com>
2022-03-22 15:05:55 -0300

    remove servicebus

--- src/main.ts
+++ src/main.ts
@@ -6,7 +6,7 @@ async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.connectMicroservice({
     strategy: new AzureBusServer({
-      url: 'Endpoint=sb://demonstracao-mensageria.servicebus.windows.net/;SharedAccessKeyName=RootManageSharedAccessKey;SharedAccessKey=KY0cdG1ZV/y1YrmfBKhOf0DtTh9C4uZE3aGpqO1ZAOo=',
+      url: '',
     }),
   });
 
